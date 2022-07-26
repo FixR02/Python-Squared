2052fcf88b3ea4fd05e28b959000692174ff074e
Conor Bradley <conorbradley321@gmail.com>
2022-07-26 19:20:08 -0400

    Establishing core script.

---
+++ python_squared.py
@@ -0,0 +1,17 @@
+print("Mic check, one-two...") #Boot-up sequence. If this all runs it should be golden
+x = 0
+import time #Importing necessary modules
+import math
+time.sleep(2) #2 second delay, to make it seem less automatic
+print("Setting up under-the-hood features...")
+time.sleep(2)
+print("Testing features...")
+time.sleep(1)
+print("Features seem to work.")
+time.sleep(1)
+input("Press any key to begin.") #Begin interactivity
+print("Welcome to Python Squared! I'm excited to be able to\nshow you the wonders of programming.")
+time.sleep(3)
+print("This is a quick tutorial sequence that will show you a few\nof Python's basics so you can get a handle on it.")
+time.sleep(2)
+print("If you find this isn't for you, that's okay! I hope this might make that decision easier.")
